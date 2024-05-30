89c2bb852c08659f76a62c9ada97d8cbd659e344
Jason21403019 <jason86100110@gmail.com>
2024-05-30 20:37:02 +0800

    新增components資料夾，並修改一點Menu.vue資料夾東西。

--- src/router/index.js
+++ src/router/index.js
@@ -29,37 +29,34 @@ const routes = [
   },
 
   // backend
+  // {
+  //   path: '/admin_login',
+  //   name: 'AdminLogin',
+  //   component: () => import('../views/AdminLogin.vue'),
+  //   meta: {
+  //     layout: 'Admin'
+  //   }
+  // },
+  // {
+  //   path: '/admin',
+  //   name: 'Admin',
+  //   component: () => import('../views/Admin.vue'),
+  //   meta: {
+  //     layout: 'Admin'
+  //   }
+  // },
+  // {
+  //   path: '/admin_account',
+  //   name: 'AdminAccount',
+  //   component: () => import('../views/AdminAccount.vue'),
+  //   meta: {
+  //     layout: 'Admin'
+  //   }
+  // },
   {
-    path: '/admin_login',
-    name: 'AdminLogin',
-    component: () => import('../views/AdminLogin.vue'),
-    meta: {
-      layout: 'Admin'
-    }
-  },
-  {
-    path: '/admin',
-    name: 'Admin',
-    component: () => import('../views/Admin.vue'),
-    meta: {
-      layout: 'Admin'
-    }
-  },
-  {
-    path: '/admin_account',
-    name: 'AdminAccount',
-    component: () => import('../views/AdminAccount.vue'),
-    meta: {
-      layout: 'Admin'
-    }
-  },
-
-
-  },
-  {
-    path: '/menu',
-    name: 'Menu',
-    component: () => import('../views/Menu.vue'),
+    path: '/menus',
+    name: 'Menus',
+    component: () => import('../views/Menus.vue'),
     meta: {
       layout: 'default'
     }
---
+++ src/views/Menus.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app" class="warper">
+    <div class="menu">
+      <menu-item v-for="item in menuItems" :key="item.title" :title="item.title" :img-path="item.imgPath" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Menus',
+  components: {
+    // MenuItem: () => import('../components/MenuItem.vue')
+  },
+  data() {
+    return {
+      menuItems: [
+        {
+          title: 'Appetizer 開胃菜',
+          imgPath: 'imgs/menuImg/food_menu_1-01.jpg'
+        },
+        {
+          title: 'Drink Snacks 喝酒菜',
+          imgPath: '../imgs/menuImg/food_menu_1-02.jpg'
+        },
+        {
+          title: "Chef's Special 主廚特選",
+          imgPath: ''
+        },
+        {
+          title: 'Captivate Intoxicat 紙醉金迷',
+          imgPath: ''
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped="scoped">
+.warper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background: $negroni;
+
+  .menu {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+  }
+}
+</style>
