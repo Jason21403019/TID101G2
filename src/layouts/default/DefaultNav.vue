06d6d7144d64a7cea5db50487849cfcef5d8abf2
Jason21403019 <jason86100110@gmail.com>
2024-05-30 10:52:47 +0800

    更新Nav

--- src/layouts/default/DefaultNav.vue
+++ src/layouts/default/DefaultNav.vue
@@ -1,24 +1,26 @@
 <template>
   <header class="header">
-    <div class="header__logo">
-      <img src="../../imgs/logo/logo-w.png" alt="" />
-    </div>
-    <nav class="header__nav">
-      <ul class="header__list">
-        <li class="header__item">
-          <router-link to="/" class="header__link">關於我們</router-link>
-          <router-link to="/" class="header__link">菜單</router-link>
-          <router-link to="/" class="header__link">預約訂位</router-link>
-          <router-link to="/" class="header__link">熱門商品</router-link>
-          <router-link to="/" class="header__link">測驗遊戲</router-link>
-          <router-link to="/" class="header__link">酒品專欄</router-link>
-          <router-link to="/" class="header__link">常見問題</router-link>
-        </li>
-      </ul>
-    </nav>
-    <div class="header__icons">
-      <router-link to="/" class="header__icons-icon">會員</router-link>
-      <router-link to="/" class="header__icons-icon">購物車</router-link>
+    <div class="header__container">
+      <div class="header__logo">
+        <img src="../../imgs/logo/logo-w.png" alt="" />
+      </div>
+      <nav class="header__nav">
+        <ul class="header__list">
+          <li class="header__item">
+            <router-link to="/" class="header__link">關於我們</router-link>
+            <router-link to="/" class="header__link">菜單</router-link>
+            <router-link to="/" class="header__link">預約訂位</router-link>
+            <router-link to="/" class="header__link">熱門商品</router-link>
+            <router-link to="/" class="header__link">測驗遊戲</router-link>
+            <router-link to="/" class="header__link">酒品專欄</router-link>
+            <router-link to="/" class="header__link">常見問題</router-link>
+          </li>
+        </ul>
+      </nav>
+      <div class="header__icons">
+        <router-link to="/" class="header__icons-icon">會員</router-link>
+        <router-link to="/" class="header__icons-icon">購物車</router-link>
+      </div>
       <div class="header__hamburgers">
         <div class="header__hamburger-bars">
           <span class="header__hamburger-bar"></span>
@@ -36,8 +38,10 @@
 .header {
   border: 1px solid red;
   background: $irishcoffee;
-  align-items: center;
-  display: flex;
+  &__container {
+    align-items: center;
+    display: flex;
+  }
   &__logo {
     width: 80px;
     height: 80px;
@@ -61,7 +65,6 @@
     border: 1px solid red;
     cursor: pointer;
     z-index: 4;
-
     &-bar {
       display: block;
       width: 25px;
