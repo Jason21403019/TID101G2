058a9e8ec2b0de06479f48fe60b55aeb5bbfd042
hsumai0202 <ac916121my2000@gmail.com>
2024-05-31 23:38:27 +0800

    新增 首頁圖片1440尺寸

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,16 @@
-<template>
-  <h1>MEi</h1>
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ad fugit doloribus commodi adipisci, molestias nobis fuga libero
-    necessitatibus repudiandae, harum exercitationem omnis explicabo accusantium optio maxime. Officia, ipsam mollitia!
-  </p>
-</template>
+  <template>
+        <section>
+            .wa
+            
+          
+        </section>
+  </template>
 
 <script>
-export default {
-  name: 'Home'
-}
+    export default {
+      name: 'Home'
+    }
 </script>
-
 <style lang="scss" scoped>
-h1 {
-  color: $irishcoffee;
-}
-p {
-  color: $irishcoffee;
-}
+  
 </style>
