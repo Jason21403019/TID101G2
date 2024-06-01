53c4060cbf3c16c6bae6b9b560ee7508b65faecd
choco <eudora001010@gmail.com>
2024-06-01 12:40:31 +0800

    feat(AdminBtn): 修改按鈕顏色

--- src/components/AdminBtnAdd.vue
+++ src/components/AdminBtnAdd.vue
@@ -1,7 +1,7 @@
 <template>
  
   <button class="create_btn" type="button" @click="handleClick">
-    <img src="../imgs/icon/icon_expand-w-67.svg" alt="" height="20px" width="20px">
+    <img src="../imgs/icon/icon_expand-w-67.svg" alt="addIcon" height="20px" width="20px">
     新增
   </button>
 
@@ -27,11 +27,17 @@
 
 <style lang="scss" scoped>
   .create_btn{
+    font-family: $fontfamily,$fontfamily-en;
+    display: flex;
+    align-items: center;
     background-color: $campari;
     color: $ramos-gin-fizz;
     border: none;
     border-radius: 4px;
-    padding: 10px;
+    padding: 5px;
+    img{
+      margin-right: 5px;
+    }
   }
 
 </style>
