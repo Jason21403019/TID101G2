611bbef58a820d26021b026dfae1d5e3de335086
choco <eudora001010@gmail.com>
2024-06-08 01:02:35 +0800

    refactor(AdminAccount): 彈跳視窗修改

--- src/views/AdminAccount.vue
+++ src/views/AdminAccount.vue
@@ -17,16 +17,6 @@
     </admin-btn>
   </div>
 
-  <!-- 彈跳視窗 -->
-  <AdminModal
-    v-model:form-data="formData"
-    :title="modalTitle"
-    :fields="formFields"
-    :visible="isModalVisible"
-    @save="handleSave"
-    @close="closeModal"
-  />
-
   <!-- 列表 -->
   <section>
     <table class="table">
@@ -47,15 +37,14 @@
           <td>{{ admin.name }}</td>
           <td>{{ admin.email }}</td>
           <td>{{ admin.phone }}</td>
-          <td>{{ admin.position }}</td>
+          <td>{{ admin.position === 'supervisor' ? '主管' : '員工' }}</td>
           <td>
             <div class="form-check form-switch">
               <input
                 :id="'flexSwitchCheckChecked' + admin.id"
-                v-model="admin.active"
                 class="form-check-input"
                 type="checkbox"
-                @change="toggleStatus(admin)"
+                v-model="admin.status"
               />
             </div>
           </td>
@@ -72,12 +61,14 @@
       </caption>
     </table>
   </section>
+
+  <ModalAccount :actionType="currentActionType" ref="modal" :admin="currentAdmin" :onSave="handleSave"></ModalAccount>
 </template>
 
 <script>
 import AdminBreadcrumb from '../components/AdminBreadcrumb.vue'
 import AdminBtn from '../components/AdminBtn.vue'
-import AdminModal from '../components/AdminModal.vue'
+import ModalAccount from '../components/AdminModalAccount.vue'
 import { variables } from '../js/AdminVariables.js'
 
 export default {
@@ -85,7 +76,7 @@ export default {
   components: {
     AdminBreadcrumb,
     AdminBtn,
-    AdminModal
+    ModalAccount
   },
   data() {
     return {
@@ -97,80 +88,48 @@ export default {
         { text: variables.adminblock.account, link: '/admin_account', active: false }
       ],
 
-      // 彈跳視窗model
-      actionType: 'add',
-      modalTitle: '管理者新增',
-      formFields: [
-        { id: 'name', label: '姓名', type: 'input', name: 'name' },
-        { id: 'email', label: 'E-mail', type: 'input' },
-        { id: 'phone', label: '手機', type: 'input' },
-        { id: 'password', label: '設定密碼', type: 'password' },
-        { id: 'confirmPassword', label: '確認密碼', type: 'password' },
-        {
-          id: 'position',
-          label: '職位',
-          type: 'select',
-          options: [
-            { value: 'manager', text: '主管' },
-            { value: 'developer', text: '員工' }
-          ]
-        },
-        { id: 'active', label: '停用/啟用', type: 'checkbox' }
-      ],
-      formData: {
+      currentActionType: 'add',
+      currentAdmin: {
         name: '',
         email: '',
         phone: '',
         password: '',
-        confirmPassword: '',
-        position: 'manager',
-        active: true
+        position: '',
+        status: false
       },
-      // 要串資料庫的地方
-      admins: [{ id: 1, name: 'Tibame', email: 'Tibame@gmail.com', phone: '09123456789', position: '主管', active: true }],
-      isModalVisible: false,
-      existingData: null
-    }
+      admins: [
+        {
+          id: 1,
+          name: 'John Doe',
+          email: 'john.doe@example.com',
+          phone: '1234567890',
+          position: 'supervisor',
+          status: true
+        },
+      ]
+    };
   },
   methods: {
-    // 彈跳視窗：是新增按鈕還是table的編輯按鈕
-    openModal(action, data = null) {
-      this.actionType = action
-      this.modalTitle = action === 'add' ? '管理者新增' : '管理者編輯'
-      this.formData = data
-        ? { ...data }
-        : {
-            name: '',
-            email: '',
-            phone: '',
-            password: '',
-            confirmPassword: '',
-            position: 'manager',
-            active: true
-          }
-      this.isModalVisible = true
-    },
-    closeModal() {
-      this.isModalVisible = false
+    openModal(action, admin = null) {
+      this.currentActionType = action;
+      this.currentAdmin = admin ? { ...admin } : { name: '', email: '', phone: '', password: '', position: '', status: false };
+      this.$refs.modal.show();
     },
     handleSave(formData) {
-      if (this.actionType === 'add') {
+      if (this.currentActionType === 'add') {
         // 新增邏輯
-        const newAdmin = { ...formData, id: this.admins.length + 1 }
-
-        this.admins.push(newAdmin)
+        const newAdmin = { ...formData, id: this.admins.length + 1 };
+        this.admins.push(newAdmin);
       } else {
         // 編輯邏輯
-        const index = this.admins.findIndex((admin) => admin.id === formData.id)
-
+        const index = this.admins.findIndex(admin => admin.id === formData.id);
         if (index !== -1) {
-          this.admins.splice(index, 1, { ...formData })
+          this.admins.splice(index, 1, { ...formData });
         }
       }
-      this.closeModal()
     }
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
@@ -180,7 +139,7 @@ export default {
   margin-top: 40px;
   margin-left: 160px;
   color: $campari;
-
+  
   &-h1 {
     display: inline;
     font-size: $fontSize_h3;
@@ -200,8 +159,7 @@ export default {
   }
 }
 
-.table td,
-.table th {
+.table td, .table th {
   vertical-align: middle;
 }
 .table {
@@ -219,6 +177,7 @@ export default {
     }
   }
   .table-tbody {
+    
     td {
       vertical-align: middle;
     }
