2f9698ad512efcce17ff7f052175252551f5d51d
choco <eudora001010@gmail.com>
2024-06-08 01:05:18 +0800

    refactor(AdminMember): 彈跳視窗修改

--- src/views/AdminMember.vue
+++ src/views/AdminMember.vue
@@ -6,6 +6,7 @@
     <span class="memberblock-pipe"> | </span>
     <h1 class="memberblock-h1">{{ subTitle }}</h1>
   </div>
+
   <!-- 搜尋 -->
   <div>
     <admin-input input-id="formGroupExampleInput1">
@@ -19,6 +20,7 @@
       </template>
     </admin-input>
 
+    <!-- 日期 -->
     <div class="d-flex align-items-center">
       <admin-date-input start-date-id="dateInputField1" end-date-id="dateInputField2">
         <template #label>選擇日期</template>
@@ -33,6 +35,7 @@
     </div>
   </div>
 
+  <!-- table欄位 -->
   <section>
     <table class="table">
       <thead class="table-thead">
@@ -48,59 +51,106 @@
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <th scope="row">M001</th>
-          <td>AAA</td>
-          <td>AAA@gmail.com</td>
-          <td>09123456789</td>
-          <td>台北市中山區南京東路三段219號5樓</td>
-          <td>2024/06/01/13:11</td>
+        <tr v-for="(member, index) in members" :key="member.id">
+          <th scope="row">{{ member.id }}</th>
+          <td>{{ member.name }}</td>
+          <td>{{ member.email }}</td>
+          <td>{{ member.phone }}</td>
+          <td>{{ member.address }}</td>
+          <td>{{ member.lastLogin }}</td>
           <td>
             <div class="form-check form-switch">
-              <input id="flexSwitchCheckChecked" class="form-check-input" type="checkbox" checked />
+              <input
+                :id="'flexSwitchCheckChecked' + member.id"
+                class="form-check-input"
+                type="checkbox"
+                v-model="member.status"
+              />
             </div>
           </td>
           <td>
-            <a href="">
+            <button @click="viewMember(member)">
               <img src="../imgs/icon/icon_admin-eye.svg" alt="" width="20px" height="20px" />
-            </a>
-          </td>
-        </tr>
-
-        <tr>
-          <th scope="row">M002</th>
-          <td>BBB</td>
-          <td>BBB@gmail.com</td>
-          <td>09123456789</td>
-          <td>台北市中山區南京東路三段219號5樓</td>
-          <td>2024/06/01/13:12</td>
-          <td>
-            <div class="form-check form-switch">
-              <input id="flexSwitchCheckChecked" class="form-check-input" type="checkbox" checked />
-            </div>
-          </td>
-          <td>
-            <a href="">
-              <img src="../imgs/icon/icon_admin-eye.svg" alt="" width="20px" height="20px" />
-            </a>
+            </button>
           </td>
         </tr>
       </tbody>
-
       <caption>
         每頁列表顯示<span class="main__list-number">6</span
         >筆
       </caption>
     </table>
   </section>
+
+  <!-- 會員彈跳 -->
+  <div
+    class="modal fade"
+    ref="memberModal"
+    id="memberModal"
+    tabindex="-1"
+    aria-labelledby="memberModalLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="memberModalLabel">會員查看</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <div class="mb-3">
+            <h4 class="col-form-label">姓名:</h4>
+            <p class="form-control">{{ currentMember.name }}</p>
+          </div>
+          <div class="mb-3">
+            <h4 class="col-form-label">生日:</h4>
+            <p class="form-control">{{ currentMember.birthday }}</p>
+          </div>
+          <div class="mb-3">
+            <h4 class="col-form-label">手機:</h4>
+            <p class="form-control">{{ currentMember.phone }}</p>
+          </div>
+          <div class="mb-3">
+            <h4 class="col-form-label">Email:</h4>
+            <p class="form-control">{{ currentMember.email }}</p>
+          </div>
+          <div class="mb-3">
+            <h4 class="col-form-label">地址:</h4>
+            <p class="form-control">{{ currentMember.address }}</p>
+          </div>
+          <div class="mb-3">
+            <h4 class="col-form-label">建立時間:</h4>
+            <p class="form-control">{{ currentMember.createdAt }}</p>
+          </div>
+          <div class="mb-3">
+            <h4 class="col-form-label">最後登入:</h4>
+            <p class="form-control">{{ currentMember.lastLogin }}</p>
+          </div>
+          <div class="mb-3">
+            <h4 class="col-form-label">停用/啟用:</h4>
+            <div class="form-check form-switch">
+              <input
+                :id="'flexSwitchCheckChecked' + currentMember.id"
+                class="form-check-input"
+                type="checkbox"
+                v-model="currentMember.status"
+              />
+            </div>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">關閉</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import AdminBreadcrumb from '../components/AdminBreadcrumb.vue'
-import AdminBtn from '../components/AdminBtn.vue'
-import AdminDateInput from '../components/AdminDateInput.vue'
 import AdminInput from '../components/AdminInput.vue'
-import Swal from 'sweetalert2'
+import AdminDateInput from '../components/AdminDateInput.vue'
+import AdminBtn from '../components/AdminBtn.vue'
 import { variables } from '../js/AdminVariables.js'
 
 export default {
@@ -119,82 +169,44 @@ export default {
         { text: '首頁', link: '/admin', active: false },
         { text: variables.memberblock.member, link: '', active: true },
         { text: variables.memberblock.memberList, link: '/admin_member', active: false }
+      ],
+      currentMember: {},
+      members: [
+        {
+          id: 'M001',
+          name: 'AAA',
+          email: 'AAA@gmail.com',
+          phone: '09123456789',
+          address: '這裡是地址',
+          lastLogin: '2024/06/01/13:11',
+          status: true,
+          birthday: '1990-01-01',
+          createdAt: '2024/01/01/12:00'
+        }
       ]
     }
   },
   methods: {
-    async search() {
-      const { value: formValues } = await Swal.fire({
-        title: '會員查看',
-        html: `
-          <form id="swal-form">
-            <div class="swal2-input-group">
-              <label for="swal-input1">姓名</label>
-              <input id="swal-input1" class="swal2-input" placeholder="請輸入姓名">
-            </div>
-            <div class="swal2-input-group">
-              <label for="swal-input2">E-mail</label>
-              <input id="swal-input2" class="swal2-input" placeholder="請輸入 E-mail">
-            </div>
-            <div class="swal2-input-group">
-              <label for="swal-input3">手機</label>
-              <input id="swal-input3" class="swal2-input" placeholder="請輸入手機號碼">
-            </div>
-            <div class="swal2-input-group">
-              <label for="swal-input4">設定密碼</label>
-              <input id="swal-input4" type="password" class="swal2-input" placeholder="如修改密碼請直接輸入,如沒有修改則無變更">
-            </div>
-            <div class="swal2-input-group">
-              <label for="swal-input5">確認密碼</label>
-              <input id="swal-input5" type="password" class="swal2-input" placeholder="如修改密碼請直接輸入,如沒有修改則無變更">
-            </div>
-            <div class="swal2-input-group">
-              <label for="swal-input6">職位</label>
-              <select id="swal-input6" class="swal2-input">
-                <option value="主管">主管</option>
-                <option value="員工">員工</option>
-              </select>
-            </div>
-            <div class="swal2-input-group">
-              <label for="swal-input7">啟用/停用</label>
-              <div class="form-check form-switch">
-                <input class="form-check-input" type="checkbox" id="swal-input7" checked>
-              </div>
-            </div>
-          </form>
-          `,
-        focusConfirm: false,
-        showCancelButton: true,
-        confirmButtonText: '儲存',
-        cancelButtonText: '取消',
-        preConfirm: () => {
-          return {
-            name: document.getElementById('swal-input1').value,
-            email: document.getElementById('swal-input2').value,
-            phone: document.getElementById('swal-input3').value,
-            password: document.getElementById('swal-input4').value,
-            confirmPassword: document.getElementById('swal-input5').value,
-            position: document.getElementById('swal-input6').value,
-            active: document.getElementById('swal-input7').checked
-          }
-        }
-      })
-
-      // if (formValues) {
-      //   Swal.fire({
-      //     icon: 'success',
-      //     title: '儲存成功',
-      //     confirmButtonText: '確定'
-      //   })
-      // }
+    viewMember(member) {
+      this.currentMember = member
+      this.showModal()
+    },
+    showModal() {
+      const modalElement = this.$refs.memberModal
+      if (modalElement) {
+        const modal = new bootstrap.Modal(modalElement)
+        modal.show()
+      } else {
+        console.error('memberModal reference is not found.')
+      }
     }
   }
 }
 </script>
-
 <style lang="scss" scoped>
 @import '../../node_modules/bootstrap/scss/bootstrap.scss'; // 確保這一行在最上面
 
+// 標題
 .memberblock {
   margin-top: 40px;
   margin-left: 160px;
@@ -209,6 +221,11 @@ export default {
   }
 }
 
+// table
+.table td,
+.table th {
+  vertical-align: middle;
+}
 .table {
   width: 85%;
   margin-top: 100px;
@@ -220,19 +237,39 @@ export default {
       color: $ramos-gin-fizz;
     }
   }
-  #flexSwitchCheckChecked:checked {
-    background-color: $toggle-on;
-    border: solid $toggle-on;
+  button {
+    border: none;
+    background: none;
   }
-  .fa-solid.fa-pencil {
-    color: $campari;
+  // table的toggle
+  .form-check-input:checked {
+    background-color: $toggle-on;
+    border-color: $toggle-on;
   }
 }
-.swal-form {
-  // z-index: 3;
-  .swal2-input {
-    pointer-events: auto; /* 確保輸入框可以被點擊和輸入 */
-    opacity: 1; /* 確保輸入框是可見的 */
+#memberModal {
+  .modal-header {
+    font-size: $fontSize_h4;
+    background-color: $babypowder;
+    color: $campari;
+  }
+  .modal-body {
+    font-size: $fontSize_h4;
+    background-color: $babypowder;
+    color: $campari;
+
+    // 彈跳視窗的toggle
+    .form-check-input:checked {
+      background-color: $toggle-on;
+      border: solid $toggle-on;
+    }
+  }
+  .modal-footer {
+    background-color: $babypowder;
+    .btn-primary {
+      background-color: $campari;
+      color: $ramos-gin-fizz;
+    }
   }
 }
 </style>
