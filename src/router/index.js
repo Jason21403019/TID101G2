04b908641e5cebf1362f0d0911d2afed274a9486
Jason21403019 <jason86100110@gmail.com>
2024-06-10 16:14:29 +0800

    修改index.js檔案各個路徑

--- src/router/index.js
+++ src/router/index.js
@@ -48,6 +48,15 @@ const routes = [
       layout: 'default'
     }
   },
+  // 訂位確認
+  {
+    path: '/reserve_sec',
+    name: 'Reserve_sec',
+    component: () => import('../views/ReserveSec.vue'),
+    meta: {
+      layout: 'default'
+    }
+  },
   // 菜單
   {
     path: '/menus',
@@ -111,124 +120,6 @@ const routes = [
       layout: 'default'
     }
   },
-  // 測驗遊戲
-  {
-    path: '/product',
-    name: 'Product',
-    component: () => import('../views/Product.vue'),
-    meta: {
-      layout: 'default'
-    }
-  },
-  // 測驗遊戲
-  {
-    path: '/quizgame',
-    name: 'Quizgame',
-    component: () => import('../views/quizgame.vue'),
-    meta: {
-      layout: 'default'
-    }
-  },
-  {
-
-    path: '/admin_login',
-    name: 'AdminLogin',
-    component: () => import('../views/AdminLogin.vue'),
-    meta: {
-      layout: 'Admin'
-    }
-  },
-  {
-    path: '/admin',
-    name: 'Admin',
-    component: () => import('../views/Admin.vue'),
-    meta: {
-      layout: 'Admin'
-    }
-  },
-  {
-    path: '/admin_member',
-    name: 'AdminMember',
-    component: () => import('../views/AdminMember.vue'),
-    meta: {
-      layout: 'Admin'
-    }
-  },
-  {
-    path: '/admin_type',
-    name: 'AdminType',
-    component: () => import('../views/AdminType.vue'),
-    meta: {
-      layout: 'Admin'
-    }
-  },
-  {
-    path: '/admin_product',
-    name: 'AdminProduct',
-    component: () => import('../views/AdminProduct.vue'),
-    meta: {
-      layout: 'Admin'
-    }
-  },
-  {
-    path: '/admin_order',
-    name: 'AdminOrder',
-    component: () => import('../views/AdminOrder.vue'),
-    meta: {
-      layout: 'Admin'
-    }
-  },
-  {
-    path: '/admin_category',
-    name: 'AdminCategory',
-    component: () => import('../views/AdminCategory.vue'),
-    meta: {
-      layout: 'Admin'
-    }
-  },
-  {
-    path: '/admin_article',
-    name: 'AdminArticle',
-    component: () => import('../views/AdminArticle.vue'),
-    meta: {
-      layout: 'Admin'
-    }
-  },
-  {
-    path: '/admin_res',
-    name: 'AdminRes',
-    component: () => import('../views/AdminRes.vue'),
-    meta: {
-      layout: 'Admin'
-    }
-  },
-  {
-    path: '/admin_account',
-    name: 'AdminAccount',
-    component: () => import('../views/AdminAccount.vue'),
-    meta: {
-      layout: 'Admin'
-    }
-  },
-  {
-    // 登入前後台頁面
-    path: '/login_page',
-    name: 'loginpage',
-    component: () => import('../views/LoginPage.vue'),
-    meta: {
-      layout: 'logindefault'
-    }
-  },
-
-  {
-    path: '/reserve_sec',
-    name: 'Reserve_sec',
-    component: () => import('../views/Reserve_sec.vue'),
-    meta: {
-      layout: 'default'
-    }
-  },
-
   // 購物車
   {
     path: '/cart',
@@ -238,20 +129,12 @@ const routes = [
       layout: 'default'
     }
   },
-  // 訂位確認
-  // {
-  //   path: '/admin_panel',
-  //   name: 'AdminPanel',
-  //   component: () => import('../views/AdminPanel.vue'),
-  //   meta: {
-  //     layout: 'Admin'
-  //   }
-  // },
 
+  // 測驗遊戲
   {
-    path: '/reserve_sec',
-    name: 'Reserve_sec',
-    component: () => import('../views/ReserveSec.vue'),
+    path: '/quizgame',
+    name: 'Quizgame',
+    component: () => import('../views/QuizGame.vue'),
     meta: {
       layout: 'default'
     }
@@ -265,12 +148,20 @@ const routes = [
       layout: 'default'
     }
   },
-
-  // 後台
+  // 後台登入
   {
     path: '/admin_login',
     name: 'AdminLogin',
     component: () => import('../views/AdminLogin.vue'),
+    meta: {
+      layout: 'Admin'
+    }
+  },
+  // 後台個人資料管理
+  {
+    path: '/admin_panel',
+    name: 'AdminPanel',
+    component: () => import('../views/AdminPanel.vue'),
     meta: {
       layout: 'Admin'
     }
@@ -293,7 +184,7 @@ const routes = [
       layout: 'Admin'
     }
   },
-  // 後台商品列表管理
+  // 後台商品列別管理
   {
     path: '/admin_type',
     name: 'AdminType',
@@ -356,7 +247,16 @@ const routes = [
       layout: 'Admin'
     }
   },
-  // 後台個人資料管理
+  {
+    // 登入前後台頁面
+    path: '/login_page',
+    name: 'loginpage',
+    component: () => import('../views/LoginPage.vue'),
+    meta: {
+      layout: 'logindefault'
+    }
+  },
+  // 訂位確認
   {
     path: '/admin_panel',
     name: 'AdminPanel',
