a020d24cea31867794fb8752c94138249949c52b
hsumai0202 <ac916121my2000@gmail.com>
2024-06-10 17:16:02 +0800

    新增會員專區的components MemberMenu及 頁面 Member

---
+++ src/components/MemberMenu.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <nav>
+      <ul>
+        <li @click="currentView = 'accountInfo'">帳戶資訊</li>
+        <li @click="currentView = 'viewOrders'">查看訂單</li>
+        <li @click="currentView = 'viewReservations'">查看預約</li>
+        <li @click="currentView = 'coupons'">優惠卷</li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MemberMenu',
+  data() {
+    return {
+      currentView: 'accountInfo'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+nav ul {
+  list-style-type: none;
+  padding: 20px 50px;
+}
+
+nav li {
+  background-color: $ramos-gin-fizz;
+  color: $blackvevet;
+  padding: 15px 16px;
+  border-bottom: 1px solid $blackvevet;
+  width: 20vw;
+  text-align: center;
+  font-size: $fontSize_h4;
+  font-family: $fontfamily;
+  font-weight: bold;
+  
+}
+
+nav li:hover {
+  color: $irishcoffee;
+}
+</style>
---
+++ src/views/Member.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="membernav">
+    <MemberMenu />
+    <!-- 會員資料 -->
+    <div class="member_account">
+      <h1>帳戶資訊</h1>
+      <form @submit.prevent="saveSettings" class="tabe_account">
+        <div class="form-row">
+          <label for="name">名字:</label>
+          <input type="text" id="name" v-model="name" required />
+        </div>
+        <div class="form-row">
+          <label for="birthdate">出生日期:</label>
+          <input type="date" id="birthdate" v-model="birthdate" required />
+        </div>
+        <div class="form-row">
+          <label for="email">信箱:</label>
+          <input type="email" id="email" v-model="email" required />
+        </div>
+        <div class="form-row">
+          <label for="phone">手機:</label>
+          <input type="tel" id="phone" v-model="phone" required />
+        </div>
+        <div class="form-row">
+          <label for="address">地址:</label>
+          <input type="text" id="address" v-model="address" required />
+        </div>
+        <!-- 按鈕 -->
+        <button type="submit">儲存設定</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import MemberMenu from '../components/MemberMenu.vue'
+
+export default {
+  name: 'Member',
+  components: {
+    MemberMenu
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.membernav {
+  background-color: $campari;
+  padding-top: 100px;
+  display: flex;
+  .member_account {
+    flex: 1;
+    margin-left: 20px;
+  }
+  h1 {
+    font-family: $fontfamily;
+    color: $ramos-gin-fizz;
+    font-size: $fontSize_h2;
+    margin-bottom: 30px;
+  }
+  // input設定
+  .tabe_account {
+    color: $ramos-gin-fizz;
+    margin-right: 40px;
+    margin-bottom: 50px;
+    display: flex;
+    flex-direction: column;
+    width: 50vw;
+  }
+  .form-row {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 10px;
+  }
+  label {
+    margin-bottom: 5px;
+  }
+  input {
+    width: auto;
+  }
+  button {
+    width: 100px;
+  }
+}
+</style>
