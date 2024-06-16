43360acbc10e5f5138452c3590b8590f295668b9
Sinxin <roy19851016@gmail.com>
2024-06-16 11:30:56 +0800

    修正打包後功能失效

--- src/views/ResultPage.vue
+++ src/views/ResultPage.vue
@@ -112,7 +112,7 @@ export default {
   async created() {
     this.userAnswers = this.parseQuery(this.$route.query.answers);
     this.result = this.$route.query.result || null;
-    
+
     if (this.result) {
       await this.setResultContent();
     }
@@ -124,48 +124,43 @@ export default {
       }
       return [];
     },
+
     async setResultContent() {
       const contentMap = {
         '歡樂與愉悅': {
           title: 'Whiskey Sour',
-          image: '../imgs/quizGameImg/Whiskey Sour.png',
+          image: new URL('../imgs/quizGameImg/Whiskey Sour.png', import.meta.url).href,
           text: 'Whiskey Sour這款的口感是層次豐富的。你可以在一口喝下之後感受到各種風味的變化，從威士忌的強烈到檸檬的清新酸感，再到糖的甜感，最後是蛋白帶來的獨特口感。這是一種相當享受口感變化的調酒，對於許多喜歡烈酒和酸味飲品的人來說，會是一個很好的選擇。'
         },
         '放鬆與舒適': {
           title: 'Mojito',
-          image: '../imgs/quizGameImg/mojito.png',
+          image: new URL('../imgs/quizGameImg/mojito.png', import.meta.url).href,
           text: '這款雞尾酒不僅帶有清爽的口感，還散發著迷人的薄荷芳香，非常適合那些喜愛探索各種調酒風格並對飲品品質有高要求的人。他們可能特別熱愛夏天的清涼感，因此這款雞尾酒是在炎熱的夏季，無論是在海邊度假，還是繁忙的一天後放鬆身心，都能帶來極大的愉悅。'
         },
         '浪漫與優雅': {
           title: 'Negroni',
-          image: '../imgs/quizGameImg/Negroni.png',
+          image: new URL('../imgs/quizGameImg/Negroni.png', import.meta.url).href,
           text: '這款雞尾酒以其強烈且獨特的風味著稱，特別適合那些已有品嚐酒精飲料經驗的成熟人士。Negroni的苦味與獨特風采，為其愛好者帶來不可多得的品飲享受。對於初次接觸雞尾酒的人來說，Negroni則可能是一段截然不同的味蕾之旅。每一口都充滿複雜且深邃的層次，讓人不禁想細細品味，回味無窮。'
         },
         '溫暖與舒心': {
           title: 'Sazerac',
-          image: '../imgs/quizGameImg/Sazerac.png',
+          image: new URL('../imgs/quizGameImg/Sazerac.png', import.meta.url).href,
           text: '這款雞尾酒的風味層次豐富，讓人一口接一口地品味。初入口時，你會首先感受到黑麥威士忌或白蘭地那深厚醇美的味道，隨後苦艾酒的苦澀與甜味悄然交織，既平衡又引人入勝。最終，一絲檸檬的酸甜恰到好處地閃現，為整體風味帶來一點細膩的變化。'
         }
       };
 
-      let content = contentMap[this.result];
+      const content = contentMap[this.result];
       if (content) {
-        try {
-          const imageModule = await import(content.image);
-          this.resultTitle = content.title;
-          this.resultImage = imageModule.default;
-          this.resultText = content.text;
-        } catch (error) {
-          console.error('Error loading image:', error);
-        }
+        this.resultTitle = content.title;
+        this.resultImage = content.image;
+        this.resultText = content.text;
       } else {
         console.error('Invalid result value:', this.result);
       }
     },
     login() {
-    this.$router.push({ path: '/about_us' });
+      this.$router.push({ path: '/register' });
+    }
   }
-  },
-  
 };
 </script>
