4e31842ec6b708fc46abc0a8b009629e109d56e2
Debbiefool <debbie0401.hrc@gmail.com>
2024-06-24 12:28:56 +0800

    訂單完成RWD

--- src/views/OrderComplete.vue
+++ src/views/OrderComplete.vue
@@ -1,213 +1,359 @@
 <template>
-    <div class="popup">
-      <div class="popup__content">
-        <div class="order">
-          <img src="../imgs/icon/icon_checkbox-w.svg" alt="" />
-          <p>感謝您的訂購，以下是您本次購物的明細</p>
-        </div>
-  
-        <div class="place">
-          <h3>紙醉金迷 台北總店</h3>
-          <p>02-70638715</p>
-          <p>地址：台北市中山區南京東路三段219號</p>
-        </div>
-  
-        <div class="data">
-          <p class="hello">張哲菘 先生/女士您好</p>
-          <p class="hello">以下是您的訂位資訊</p>
-          <p class="r_date">2024年6月11日 星期二</p>
-          <p class="r_time">12:00</p>
-          <p class="r_guests">用餐人數 12 位</p>
-  
-         
-  
-          <button>取消訂位</button>
-  
-          <div class="btn_text">
-            <p>如需更改訂位資訊，或有其他訂位問題，請來電</p>
+  <div class="popup">
+    <div class="popup__content">
+      <div class="order">
+        <img src="../imgs/icon/icon_checkbox-w.svg" alt="" />
+        <p>感謝您的訂購，以下是您本次購物的明細</p>
+      </div>
+
+      <div class="place">
+        <h3>訂單編號：2057382977594</h3>
+      </div>
+      
+    <div class="scroll_container">
+      <div class="product">
+          <div class="item_details">
+        <!-- 標題&數量按鈕 -->
+              <div class="text">
+                <h5>Le Petit Chavin 小夏凡 夏多內無酒精氣泡葡萄飲</h5>
+                <div class="text_down">
+                    <p>750ml/瓶</p>
+                </div>
+              </div> 
           </div>
-        </div>
+          <div class="price">
+              <p>NT$ 890</p>
+          </div>     
+      </div>
+
+      <div class="product">
+          <div class="item_details">
+        <!-- 標題&數量按鈕 -->
+              <div class="text">
+                <h5>Le Petit Chavin 小夏凡 夏多內無酒精氣泡葡萄飲</h5>
+                <div class="text_down">
+                    <p>750ml/瓶</p>
+                </div>
+              </div> 
+          </div>
+          <div class="price">
+              <p>NT$ 890</p>
+          </div>     
+      </div>
+
+      <div class="product">
+          <div class="item_details">
+        <!-- 標題&數量按鈕 -->
+              <div class="text">
+                <h5>Le Petit Chavin 小夏凡 夏多內無酒精氣泡葡萄飲</h5>
+                <div class="text_down">
+                    <p>750ml/瓶</p>
+                </div>
+              </div> 
+          </div>
+          <div class="price">
+              <p>NT$ 890</p>
+          </div>     
       </div>
     </div>
-  </template>
+
+    <div class="order_info">
+      <!-- <h3>收件資訊</h3> -->
+      <h4>收件人:鄭卉汝</h4>
+      <h4>聯絡電話:0912345678</h4>
+      <h4>地址:台北市中山區南京東路三段219號</h4>
+    </div>
+
+    <div class="pay_info">
+
+          <div class="status">
+              <h5>付款狀態</h5>
+              <p>已付款</p>
+          </div>
+
+          <div class="pay_method">
+              <h5>付款方式</h5>
+              <p>信用卡</p>
+          </div>
+
+          <div class="ship">
+              <h5>送貨方式</h5>
+              <p>宅配</p>
+          </div>
+
+          <div class="subtotal">
+              <h5>小計</h5>
+              <p>$45000</p>
+          </div>
+
+          <div class="ship_fee">
+              <h5>運費</h5>
+              <p>$250</p>
+          </div>
+
+          <div class="ship_discount">
+              <h5>折抵</h5>
+              <p>-$250</p>
+          </div>
+
+          <div class="total_price">
+              <h5>總計額</h5>
+              <p class="twd">TWD $45000</p>
+          </div>
+
+    </div>
+
+
+
+    </div>
+
+  </div>
+</template>
 
 <style lang="scss" scoped>
 .popup {
-  padding: 50px;
-  width: 50%;
-  margin: 0 auto;
-  margin-top: 100px;
+padding: 50px;
+width: 50%;
+margin: 0 auto;
+margin-top: 100px;
 
-  @include breakpoint(1280px) {
-    width: 60%;
+@include breakpoint(1280px) {
+  width: 60%;
+}
+
+@include breakpoint(820px) {
+    width: 80%;
+}
+
+@include breakpoint(540px) {
+    width: 100%;
+}
+
+@include breakpoint(430px){
+    padding: 20px;
+}
+
+@include breakpoint(375px){
+     padding: 10px;
+}
+
+p {
+  font-family: $fontfamily;
+  color: $campari;
+  //  text-align: center;
+}
+
+.popup__content {
+  background-color: $ramos-gin-fizz;
+  padding: 0 0 50px 0;
+
+  @include breakpoint(430px) {
+    padding: 0 0 30px 0;
   }
 
-  @include breakpoint(820px) {
-      width: 80%;
-  }
-
-  @include breakpoint(540px) {
-      width: 100%;
-  }
-
-  @include breakpoint(430px){
-      padding: 20px;
-  }
-
-  @include breakpoint(375px){
-       padding: 10px;
-  }
-
-  p {
-    font-family: $fontfamily;
-    color: $campari;
-    //  text-align: center;
-  }
-
-  .popup__content {
-    background-color: $ramos-gin-fizz;
-    padding: 0 0 50px 0;
+  .order {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: $campari;
+    border-bottom: 10px solid #fff;
+    padding: 20px;
 
     @include breakpoint(430px) {
-      padding: 0 0 30px 0;
+      padding: 10px;
     }
 
-    .order {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      background-color: $campari;
-      border-bottom: 10px solid #fff;
-      padding: 20px;
-
-      @include breakpoint(430px) {
-        padding: 10px;
-      }
-
-      img {
-        width: 30px;
-        height: 30px;
-        margin-right: 5px;
-      }
-
-      p {
-        line-height: 2;
-        color: $ramos-gin-fizz;
-        letter-spacing: $letterspacing;
-        font-size: $fontSize_h4;
-      }
-    }
-    .place {
-      // outline: 1px solid palegreen;
-      text-align: center;
-      padding: 20px;
+    img {
+      width: 30px;
+      height: 30px;
+      margin-right: 5px;
 
       @include breakpoint(430px){
-          padding: 10px;
+          width: 20px;
+          height: 20px;
+          margin-right: 10px;
       }
-
-      h3 {
-        font-size: $fontSize_h3;
-        font-family: $fontfamily;
-        color: $campari;
-        margin-bottom: 10px;
-      }
-
-      p {
-        font-size: $fontSize_h5;
-        margin-bottom: 10px;
-      
-
-        @include breakpoint(430px){
-            margin-bottom: 0;
-        }
-
-        @include breakpoint(375px){
-          font-size: $fontSize_p;
-          
-        }
-
-      }
-
     }
 
-    .data {
-      width: 80%;
-      margin: 0 auto;
-      border: 1px solid $irishcoffee;
-      padding: 30px;
-      text-align: center;
+    p {
+      line-height: 2;
+      color: $ramos-gin-fizz;
+      letter-spacing: $letterspacing;
+      font-size: $fontSize_h4;
 
-        @include breakpoint(430px) {
-            padding: 10px;
-        }
-
-        @include breakpoint(375px){
-            width: 100%;
-        }
-
-      .hello {
-        font-size: $fontSize_p;
-        margin-bottom: 10px;
-
-        @include breakpoint(430px) {
-          font-size: $fontSize_p;
-        }
+      @include breakpoint(768px) {
+      font-size: $fontSize_h5;
       }
 
-      .r_date {
-        font-size: $fontSize_h3;
-        margin-bottom: 30px;
-        font-weight: bold;
-
-        @include breakpoint(430px){
-          font-size: 1.5rem;
-        }
+      @include breakpoint(430px) {
+      font-size: $fontSize_p;
+      line-height: 1.5;
       }
 
-      .r_time {
-        font-size: $fontSize_h1;
-        color: rgb(248, 34, 34);
-        margin-bottom: 30px;
-        font-weight: bold;
-      }
-
-      .r_guests {
-        font-size: $fontSize_p;
-        margin-bottom: 30px;
-
-        @include breakpoint(430px) {
-          font-size: $fontSize_h4;
-        }
-      }
-
-      button {
-        width: 35%;
-        color: $irishcoffee;
-        font-size: $fontSize_h4;
-        background-color: $ramos-gin-fizz;
-        border: 1px solid $irishcoffee;
-        border-radius: 5px;
-        line-height: 2;
-        margin-bottom: 30px;
-        font-weight: bold;
-
-          @include breakpoint(430px){
-            width: 40%;
-          }
-       }
-
-      .btn_text {
-        p {
-          font-size: $fontSize_p;
-
-          @include breakpoint(430px) {
-            font-size: $fontSize_h5;
-            line-height: 2;
-          }
-        }
-      }
     }
   }
+  .place {
+    // outline: 1px solid palegreen;
+    text-align: center;
+    padding: 20px;
+
+    @include breakpoint(430px){
+        padding: 10px;
+    }
+
+    h3 {
+      font-size: $fontSize_h3;
+      font-family: $fontfamily;
+      color: $campari;
+      margin-bottom: 10px;
+
+      @include breakpoint(430px){
+          font-size: 1.5rem;
+      }
+    }
+
+
+  }
+
+  .scroll_container {
+
+      overflow-y: scroll;
+      width: 85%;
+      height: 300px;
+      margin: 0 auto;
+      margin-bottom: 30px;
+    
+  
+    .product {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 10px;
+      border:1px solid $campari;
+      border-radius: 4px;
+      background-color: $whitelady;
+      width: 95%;
+      margin: 0 auto;
+      margin-bottom: 20px;
+
+      @include breakpoint(390px){
+        width: 100%;
+      }
+
+      h5{
+        font-family:$fontfamily;
+        font-size: $fontSize_h5;
+
+        @include breakpoint(390px){
+          font-size: $fontSize_p;
+        }
+      }
+
+      p{
+        font-family:$fontfamily;
+        font-size: $fontSize_p;
+      }
+        .item_details{
+          // outline: 1px solid red;
+          width: 100%;
+
+          .text_down{
+            margin-top: 10px;
+          }
+        }
+        .price{
+          width: 30%;
+          //  outline:1px solid red;
+          margin-left: 20px;
+          text-align: center;
+
+          @include breakpoint(390px){
+            // margin-left: 0;
+            width: 40%;
+          }
+        }
+    }
+
+  }
+
+  .order_info{
+
+    width: 85%;
+    margin: 0 auto;
+    padding: 10px;
+    border-top: 1px dashed $campari;
+    border-bottom: 1px dashed $campari;
+    // border-radius: 4px 4px 0 0;
+
+    h4{
+      font-family: $fontfamily;
+      font-size: $fontSize_p;
+      color:$campari;
+      letter-spacing:$letterspacing;
+      // font-weight: bold;
+    }
+  }
+
+  .pay_info{
+    // outline: 1px solid red;
+    width: 85%;
+    margin: 0 auto;
+    padding: 10px;
+    border-bottom:1px dashed $campari;
+  
+    // border-radius:0 0 4px 4px;
+
+    h5{
+      font-family: $fontfamily;
+      font-size: $fontSize_p;
+      letter-spacing:$letterspacing;
+      color: $campari;
+      // font-weight: bold;
+    }
+
+    p{
+      font-family: $fontfamily;
+      font-size: $fontSize_p;
+      color: $campari;
+      letter-spacing:3px;
+      
+    }
+   
+    .status{
+      display: flex;
+      justify-content: space-between;
+    }
+
+    .pay_method{
+      display: flex;
+      justify-content: space-between;
+    }
+
+    .ship{
+      display: flex;
+      justify-content: space-between;
+    }
+
+    .subtotal{
+      display: flex;
+      justify-content: space-between;
+    }
+
+    .ship_fee{
+      display: flex;
+      justify-content: space-between;
+    }
+
+    .ship_discount{
+      display: flex;
+      justify-content: space-between;
+    }
+
+    .total_price{
+      display: flex;
+      justify-content: space-between;
+    }
+  }
+}
 }
 </style>
