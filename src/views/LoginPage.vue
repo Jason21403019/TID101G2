0bc7782e947972dfa26029784b76d5a55f8015ac
hsumai0202 <ac916121my2000@gmail.com>
2024-07-04 23:31:16 +0800

    LoginPage > 將酒杯改為背景圖

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -11,8 +11,8 @@
             <img class="log_admin" src="../imgs/loginImg/login_but-03.svg" width="125" height="125" />
           </a>
         </div>
-        <div class="login">
-          <img :src="onImage" width="590" />
+        <div class="login" :style="backgroundStyle">
+          <!-- Empty div used for background image -->
         </div>
       </div>
     </div>
@@ -25,16 +25,31 @@ export default {
   data() {
     return {
       onImage: new URL(`@/imgs/loginImg/login-on.jpg`, import.meta.url).href,
-      offImage: new URL(`@/imgs/loginImg/login-off.jpg`, import.meta.url).href
+      offImage: new URL(`@/imgs/loginImg/login-off.jpg`, import.meta.url).href,
+      currentImage: '' // Current background image URL
+    }
+  },
+  computed: {
+    backgroundStyle() {
+      return {
+        backgroundImage: `url(${this.currentImage})`,
+        backgroundSize: 'cover', // Cover the entire div
+        backgroundPosition: 'center', // Center the background image
+        width: '100%', // Full width
+        height: '100vh' // Full viewport height
+      }
     }
   },
   methods: {
     handleClick(link) {
-      this.onImage = this.offImage // 更换图片以显示动画或视觉反馈
+      this.currentImage = this.offImage // Switch to offImage as background
       setTimeout(() => {
-        this.$router.push(link) // 跳转到相应页面
-      }, 300) // 短暂延迟以显示动画效果
+        this.$router.push(link) // Navigate after a short delay
+      }, 300)
     }
+  },
+  mounted() {
+    this.currentImage = this.onImage // Set initial background image
   }
 }
 </script>
@@ -52,14 +67,6 @@ section {
   .login {
     text-align: center;
     overflow: hidden;
-    img {
-      display: flex;
-      @media (max-width: 767px) {
-        margin: 0 auto;
-        display: block;
-        width: 100vw;
-      }
-    }
   }
   .log_home,
   .log_admin {
