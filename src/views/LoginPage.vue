12cb7a5dcf22405955b25b6eb474ffa67e0e64f4
hsumai0202 <ac916121my2000@gmail.com>
2024-07-05 11:00:00 +0800

    LoginPage > 修改圖片問題

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -6,13 +6,13 @@
           <a @click="handleClick('/home')" data-link="/home">
             <img class="log_home" src="../imgs/loginImg/login_but-02.svg" width="125" height="125" />
           </a>
-          <p style="color: #fff">點擊冰塊進入頁面</p>
+          <p style="color: #fff">點擊冰快進入頁面</p>
           <a @click="handleClick('/admin_login')" data-link="/admin_login">
             <img class="log_admin" src="../imgs/loginImg/login_but-03.svg" width="125" height="125" />
           </a>
         </div>
-        <div class="login" :style="backgroundStyle">
-          <!-- Empty div used for background image -->
+        <div class="login">
+          <img :src="onImage" width="590" />
         </div>
       </div>
     </div>
@@ -25,31 +25,17 @@ export default {
   data() {
     return {
       onImage: new URL(`@/imgs/loginImg/login-on.jpg`, import.meta.url).href,
-      offImage: new URL(`@/imgs/loginImg/login-off.jpg`, import.meta.url).href,
-      currentImage: '' // Current background image URL
+      offImage: new URL(`@/imgs/loginImg/login-off.jpg`, import.meta.url).href
     }
   },
-  computed: {
-    backgroundStyle() {
-      return {
-        backgroundImage: `url(${this.currentImage})`,
-        backgroundSize: 'cover', // Cover the entire div
-        backgroundPosition: 'center', // Center the background image
-        width: '100%', // Full width
-        height: '100vh' // Full viewport height
-      }
-    }
-  },
+  //跳轉換圖設定
   methods: {
     handleClick(link) {
-      this.currentImage = this.offImage // Switch to offImage as background
+      this.onImage = this.offImage
       setTimeout(() => {
-        this.$router.push(link) // Navigate after a short delay
+        this.$router.push(link)
       }, 300)
     }
-  },
-  mounted() {
-    this.currentImage = this.onImage // Set initial background image
   }
 }
 </script>
@@ -58,20 +44,40 @@ export default {
 section {
   background: #000;
   height: 100vh;
+
   .wrapper {
     display: flex;
     justify-content: space-evenly;
-    padding-top: 100px;
+    padding-top: 40px;
     z-index: 1;
+    @media (max-width: 767px) {
+      margin: 0 auto;
+      flex-direction: column;
+      align-items: center;
+      // width: 80vw;
+    }
   }
+
   .login {
     text-align: center;
-    overflow: hidden;
+    img {
+      position: relative;
+      @media (max-width: 767px) {
+        margin: 0 auto;
+        display: block;
+        width: 100vw;
+      }
+    }
   }
+
   .log_home,
   .log_admin {
     cursor: pointer;
     transition: transform 0.3s ease;
+    @media (max-width: 767px) {
+      width: 90%;
+      margin-bottom: 20px;
+    }
   }
 
   .log_home:hover,
